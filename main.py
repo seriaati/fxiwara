63a0481621e9fcc7fca26d8c81cf8db5be272a38
seriaati <seria.ati@gmail.com>
2024-09-12 10:01:31 +0800

    Use requests instead of httpx

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from urllib.parse import unquote
 
 import fastapi
 import httpx
+import requests
 import uvicorn
 
 app = fastapi.FastAPI()
@@ -36,29 +37,22 @@ async def video_proxy(
 
 
 @app.get("/video/{video_id}/{video_name}")
-async def video_endpoint(
-    video_id: str, video_name: str
-) -> fastapi.responses.HTMLResponse:
+def video_endpoint(video_id: str, video_name: str) -> fastapi.responses.HTMLResponse:
     url = f"https://iwara.tv/video/{video_id}/{video_name}"
     api_url = f"https://api.iwara.tv/video/{video_id}"
 
-    async with httpx.AsyncClient() as client:
-        response = await client.get(api_url)
-        data = response.json()
+    response = requests.get(api_url)
+    data = response.json()
 
-        file_url = data["fileUrl"]
+    file_url = data["fileUrl"]
+    file_response = requests.get(
+        file_url, headers={"x-version": "cc8b2b7d31592a95c1701fb0fb32b04d78e4de32"}
+    )
+    file_data = file_response.json()
 
-        file_response = await client.get(
-            file_url,
-            headers={
-                "x-version": "cc8b2b7d31592a95c1701fb0fb32b04d78e4de32",
-            },
-        )
-        file_data = file_response.json()
-
-        p360 = next(d for d in file_data if d["name"] == "360")
-        video_url = "https:" + p360["src"]["download"]
-        proxy_url = f"https://fxiwara.seria.moe/proxy?url={video_url}"
+    p360 = next(d for d in file_data if d["name"] == "360")
+    video_url = "https:" + p360["src"]["download"]
+    proxy_url = f"https://fxiwara.seria.moe/proxy?url={video_url}"
 
     html = f"""
     <html>
@@ -70,8 +64,8 @@ async def video_endpoint(
         <meta property="og:type" content="video">
         <meta property="og:site_name" content="👁️ Views: {data['numViews']}\n👍 Likes: {data['numLikes']}">
         <meta property="og:url" content="{url}">
-        <meta property="og:video" content="{video_url}">
-        <meta property="og:video:secure_url" content="{video_url}">
+        <meta property="og:video" content="{proxy_url}">
+        <meta property="og:video:secure_url" content="{proxy_url}">
         <meta property="og:video:type" content="video/mp4">
         
         <script>
@@ -83,7 +77,7 @@ async def video_endpoint(
     
     <body>
         <p>Redirecting you to the Iwara video...</p>
-        <p>If you are not redirected automatically, <a href="{url}">click here</a>.</p>
+        <p>If you are not redirected automatically, <a href="{proxy_url}">click here</a>.</p>
     </body>
     
     </html>
